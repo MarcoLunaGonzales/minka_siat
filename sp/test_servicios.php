b7fcb3d880aafca88d5f094267f8606fe2240174
Marco Laptop <lunagonzalesmarco@gmail.com>
2022-08-04 20:49:07 -0400

    cambios servicios

--- sp/test_servicios.php
+++ sp/test_servicios.php
@@ -3,32 +3,6 @@
 $sIde = "MinkaSw123*";
 $sKey = "rrf656nb2396k6g6x44434h56jzx5g6";
 
-/*PARAMETROS PARA EJECUTAR LAS OPERACIONES*/ 
-  // $parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, 
-  //           "accion"=>"registrarCiudad", //Nuevo contacto de empresa
-  //           "idEmpresa"=>2, //ID de empresa, otorgado por minkasoftware
-  //           "nitEmpresa"=>'10916889016', //nit  de empresa
-  //           "codigoExterno"=>1, //codigo de ciudad de cliente
-  //           "nombreCiudad"=>"Casa Matriz - 0", //nombre ciudad
-  //           "codigoImpuestos"=>0, //codigo de impuestos nacionales
-  //           "direccionCiudad"=>"Zona Bajo Irpavi", //direccion de ciudad
-  //           "codigoActividadEconomica"=>475100, //actividad economica
-  //           );  
-  // $datos=json_encode($parametros);
-  // // abrimos la sesión cURL
-  // $ch = curl_init();
-  // // definimos la URL a la que hacemos la petición  
-  // curl_setopt($ch, CURLOPT_URL,"http://localhost:8080/SistemaFacturacion/wsminka/registro/ws_ciudades.php"); // local
-  // // indicamos el tipo de petición: POST
-  // curl_setopt($ch, CURLOPT_POST, TRUE);
-  // // definimos cada uno de los parámetros
-  // curl_setopt($ch, CURLOPT_POSTFIELDS, $datos);
-  // // recibimos la respuesta y la guardamos en una variable
-  // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  // $remote_server_output = curl_exec ($ch);
-  // // cerramos la sesión cURL
-  // curl_close ($ch);
-
 ///verificar conexion
   // $parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, 
   //           "accion"=>"verificarComunicacion", //Nuevo contacto de empresa
@@ -53,11 +27,11 @@ $sKey = "rrf656nb2396k6g6x44434h56jzx5g6";
 
 
   //Lista de Tipos de Pago
-  // $parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, 
-  //           "accion"=>"sincronizarParametricaTipoMetodoPago", //
-  //           "idEmpresa"=>2, //ID de empresa, otorgado por minkasoftware
-  //           "nitEmpresa"=>'10916889016' //nit  de empresa
-  //         );  
+// $parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, 
+//           "accion"=>"sincronizarParametricaTipoMetodoPago", //
+//           "idEmpresa"=>2, //ID de empresa, otorgado por minkasoftware
+//           "nitEmpresa"=>'10916889016' //nit  de empresa
+//         );  
   //Lista de Tipos documento
     $parametros=array("sIdentificador"=>$sIde, "sKey"=>$sKey, 
             "accion"=>"sincronizarParametricaTipoDocumentoIdentidad", //
@@ -69,7 +43,7 @@ $sKey = "rrf656nb2396k6g6x44434h56jzx5g6";
     // abrimos la sesión cURL
     $ch = curl_init();
     // definimos la URL a la que hacemos la petición  
-    curl_setopt($ch, CURLOPT_URL,"http://localhost:8080/minka_siat/wsminka/ws_operaciones.php"); // local
+    curl_setopt($ch, CURLOPT_URL,"http://localhost:8090/minka_siat/wsminka/ws_operaciones.php"); // local
     // indicamos el tipo de petición: POST
     curl_setopt($ch, CURLOPT_POST, TRUE);
     // definimos cada uno de los parámetros
