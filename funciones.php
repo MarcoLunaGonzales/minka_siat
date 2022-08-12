0b5f7949365b124c3728f662c897d77efd744294
Marco Laptop <lunagonzalesmarco@gmail.com>
2022-08-12 17:33:17 -0400

    cambiso

--- funciones.php
+++ funciones.php
@@ -1,4 +1,17 @@
 <?php
+
+function callService($parametros, $url){
+$parametros=json_encode($parametros);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_POST, TRUE);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $parametros);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$remote_server_output = curl_exec ($ch);
+curl_close ($ch);
+return $remote_server_output;   
+}
+
 function obtenerValorConfiguracion($enlaceCon,$id){
 	$estilosVenta=1;
 	//require("conexionmysqli2.inc");
