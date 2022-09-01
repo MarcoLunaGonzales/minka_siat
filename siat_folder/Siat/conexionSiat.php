73e69fb91ee5dfa48173e070229e73ad85bc5866
isullcamani <sullcamani@gmail.com>
2022-09-01 16:11:39 -0400

    conection bd solution

--- siat_folder/Siat/conexionSiat.php
+++ siat_folder/Siat/conexionSiat.php
@@ -4,12 +4,26 @@
 // 	$globalEntidad=$_COOKIE['globalIdEntidad'];
 // 	switch ($globalEntidad) {
 // 		case 2://Entidad prueba
-			$siat_nombreSistema='uniloyola v1';
-			$siat_codigoSistema='721CFFC51FA7DC9C67E0DA6';
-			$siat_tipo='PROPIO';
-			$siat_nit=315910027;
-			$siat_razonSocial='UNIVERSIDAD LOYOLA DE BOLIVIA SOCIEDAD ANONIMA';
-			$siat_tokenDelegado='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJMb3lvbGFTQTIwMTYiLCJjb2RpZ29TaXN0ZW1hIjoiNzIxQ0ZGQzUxRkE3REM5QzY3RTBEQTYiLCJuaXQiOiJINHNJQUFBQUFBQUFBRE0yTkxVME5EQXdNZ2NBc3hvd2ZBa0FBQUE9IiwiaWQiOjY2ODE1NSwiZXhwIjoxNjkyODM1MjAwLCJpYXQiOjE2NjEzOTUyMzgsIm5pdERlbGVnYWRvIjozMTU5MTAwMjcsInN1YnNpc3RlbWEiOiJTRkUifQ.qwvgqc-zTu_tOzos9_VG2-3s2mg1GwQYr1fX1Wokypuus5N4o_age-Sdpvtl2FPun-hbygZD7sfC5PqvxPLIlQ';
+				
+			// $siat_nombreSistema='uniloyola v1';
+			// $siat_codigoSistema='721CFFC51FA7DC9C67E0DA6';
+			// $siat_tipo='PROPIO';
+			// $siat_nit=315910027;
+			// $siat_razonSocial='UNIVERSIDAD LOYOLA DE BOLIVIA SOCIEDAD ANONIMA';
+			// $siat_tokenDelegado='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJMb3lvbGFTQTIwMTYiLCJjb2RpZ29TaXN0ZW1hIjoiNzIxQ0ZGQzUxRkE3REM5QzY3RTBEQTYiLCJuaXQiOiJINHNJQUFBQUFBQUFBRE0yTkxVME5EQXdNZ2NBc3hvd2ZBa0FBQUE9IiwiaWQiOjY2ODE1NSwiZXhwIjoxNjkyODM1MjAwLCJpYXQiOjE2NjEzOTUyMzgsIm5pdERlbGVnYWRvIjozMTU5MTAwMjcsInN1YnNpc3RlbWEiOiJTRkUifQ.qwvgqc-zTu_tOzos9_VG2-3s2mg1GwQYr1fX1Wokypuus5N4o_age-Sdpvtl2FPun-hbygZD7sfC5PqvxPLIlQ';
+// 	error_reporting(E_ALL);
+// ini_set('display_errors', '1');
+
+			require dirname(__DIR__). SB_DS ."../conexionmysqli2.php";
+			$consulta="select nombre_sistema,codigo_sistema,tipo_sistema,nit,razon_social,token_delegado,fecha_limite from siat_credenciales where cod_estado=1";
+			$respFactura = mysqli_query($enlaceCon,$consulta);
+			$dataFact = $respFactura->fetch_array(MYSQLI_ASSOC);			
+			$siat_nombreSistema = $dataFact['nombre_sistema'];			
+			$siat_codigoSistema=$dataFact['codigo_sistema'];
+			$siat_tipo=$dataFact['tipo_sistema'];
+			$siat_nit=$dataFact['nit'];
+			$siat_razonSocial=$dataFact['razon_social'];
+			$siat_tokenDelegado=$dataFact['token_delegado'];
 // 		break;
 // 		default:
 // 			$siat_nombreSistema='';
@@ -23,19 +37,4 @@
 // 	}
 // }
 
-
-// $siat_nombreSistema='TuAdmin';
-// $siat_codigoSistema='71E6551344591ADE1082FA6';
-// $siat_tipo='PROPIO';
-// $siat_nit=120361029;
-// $siat_razonSocial='FARMACIA CARMELITA S.R.L.';
-// $siat_tokenDelegado='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJDYXJtZWxpdGEyOSIsImNvZGlnb1Npc3RlbWEiOiI3MUU2NTUxMzQ0NTkxQURFMTA4MkZBNiIsIm5pdCI6Ikg0c0lBQUFBQUFBQUFETTBNakEyTXpRd3NnUUFaYV9Pd2drQUFBQT0iLCJpZCI6MTM3NTk5LCJleHAiOjE2NzI1MzEyMDAsImlhdCI6MTY0ODUxMjgzMywibml0RGVsZWdhZG8iOjEyMDM2MTAyOSwic3Vic2lzdGVtYSI6IlNGRSJ9.-6f2wYcvrnCuUql7vk2V6O-jouRVwTZoB83-Ms8Vxk_sivWA2cAnW2Ix4yS1lm6eBPeOZPzZcoQ0EkCcIOXVMA';
-
-
-// $siat_nombreSistema='SISTEMA COMERCIAL';
-// $siat_codigoSistema='720F3D79F78914E5071CB36';
-// $siat_tipo='PROPIO';
-// $siat_nit=3463783013;
-// $siat_razonSocial='PEREIRA BIANCHI PATRICIA GIANELLA';
-// $siat_tokenDelegado='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJSb211bG81MCIsImNvZGlnb1Npc3RlbWEiOiI3MjBGM0Q3OUY3ODkxNEU1MDcxQ0IzNiIsIm5pdCI6Ikg0c0lBQUFBQUFBQUFETTJNVE0ydHpBMk1EUUdBSTgxN1A0S0FBQUEiLCJpZCI6MzA3OTUsImV4cCI6MTY4NzQ3ODQwMCwiaWF0IjoxNjU2MDM2NjQ2LCJuaXREZWxlZ2FkbyI6MzQ2Mzc4MzAxMywic3Vic2lzdGVtYSI6IlNGRSJ9.R5xqz2kDUly0UTUpET1pzloxrALjQ9EAbNrHkas99y09vVco0pdLJqboikBJ4RFHIGpy0DBRjJ1vlqd96OPCIA';
 
