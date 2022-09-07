2251a99563fcaa03ad12c46257d8c3f7343d4219
isullcamani <sullcamani@gmail.com>
2022-09-07 15:06:46 -0400

    cambios en facturacion

--- funciones_inventarios.php
+++ funciones_inventarios.php
@@ -75,7 +75,7 @@ function insertar_detalleSalidaVenta($enlaceCon,$cod_salida, $cod_almacen, $cod_
 	$sqlInsert="insert into salida_detalle_almacenes (cod_salida_almacen, cod_material, cantidad_unitaria, lote, fecha_vencimiento, precio_unitario,
 	descuento_unitario, monto_unitario, orden_detalle,observaciones) values ('$cod_salida', '$cod_material', '$cantidad', '0', '0000-00-00',
 	'$precio','$descuento','$montoparcial','$orden','$concepto')";
-	
+	echo "<br>detalle:".$sqlInsert."<br>";
 	$respInsert=mysqli_query($enlaceCon,$sqlInsert);
 	if($respInsert!=1){
 		$banderaError=2;
