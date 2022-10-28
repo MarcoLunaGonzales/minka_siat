8f93d43fef99173209fc56e8a6732fcc37a63dca
Marco Laptop <lunagonzalesmarco@gmail.com>
2022-10-27 23:16:11 -0400

    cambios cufd

--- siat_folder/Siat/conexionSiatUrl.php
+++ siat_folder/Siat/conexionSiatUrl.php
@@ -3,13 +3,13 @@ namespace SinticBolivia\SBFramework\Modules\Invoices\Classes\Siat;
 class conexionSiatUrl 
 {
 	//pruebas
-	const endpoint 	= 'https://pilotosiatservicios.impuestos.gob.bo/v22/ServicioFacturacionComputarizada';
-	const wsdl 		= 'https://pilotosiatservicios.impuestos.gob.bo/v22/ServicioFacturacionComputarizada?wsdl';
-	const wsdlSincronizacion = 'https://pilotosiatservicios.impuestos.gob.bo/v22/FacturacionSincronizacion?wsdl';
-	const wsdlCodigo='https://pilotosiatservicios.impuestos.gob.bo/v22/FacturacionCodigos?wsdl';
-	const wsdlFacturacionElectronica='https://pilotosiatservicios.impuestos.gob.bo/v22/ServicioFacturacionElectronica?wsdl';
-	const wsdlOperaciones = 'https://pilotosiatservicios.impuestos.gob.bo/v22/FacturacionOperaciones?wsdl';
-	const wsdlCompraVenta='https://pilotosiatservicios.impuestos.gob.bo/v22/ServicioFacturacionComputarizada?wsdl';
+	const endpoint 	= 'https://pilotosiatservicios.impuestos.gob.bo/v2/ServicioFacturacionComputarizada';
+	const wsdl 		= 'https://pilotosiatservicios.impuestos.gob.bo/v2/ServicioFacturacionComputarizada?wsdl';
+	const wsdlSincronizacion = 'https://pilotosiatservicios.impuestos.gob.bo/v2/FacturacionSincronizacion?wsdl';
+	const wsdlCodigo='https://pilotosiatservicios.impuestos.gob.bo/v2/FacturacionCodigos?wsdl';
+	const wsdlFacturacionElectronica='https://pilotosiatservicios.impuestos.gob.bo/v2/ServicioFacturacionElectronica?wsdl';
+	const wsdlOperaciones = 'https://pilotosiatservicios.impuestos.gob.bo/v2/FacturacionOperaciones?wsdl';
+	const wsdlCompraVenta='https://pilotosiatservicios.impuestos.gob.bo/v2/ServicioFacturacionComputarizada?wsdl';
 
 	//Oficial
 	// const endpoint 	= 'https://siatrest.impuestos.gob.bo/v2/ServicioFacturacionComputarizada';
