18452391305f2ba22ffeab5ee8d5e2de2d8602ef
Marco Laptop <lunagonzalesmarco@gmail.com>
2023-01-09 16:08:01 -0400

    cambios reportes siat

--- rptLibroVentastxt_siatv2.php
+++ rptLibroVentastxt_siatv2.php
@@ -33,40 +33,42 @@ if($tipo>0){
 	}	
 }
 
-/*$sql="select f.nro_factura, DATE_FORMAT(f.fecha, '%d/%m/%Y'), s.monto_final, f.razon_social, f.nit, s.siat_cuf as nro_autorizacion, e.abreviatura, f.codigo_control,(SELECT descripcion FROM ciudades where cod_ciudad=f.cod_sucursal)nombre_ciudad,s.cod_tipo_doc,f.cod_estado
-	from facturas_venta f, estados_factura e,salida_almacenes s
-	where  e.cod_estado=f.cod_estado and s.cod_salida_almacenes=f.cod_venta
-	and YEAR(f.fecha)=$codAnio and MONTH(f.fecha)=$codMes and f.cod_sucursal in ($rpt_territorio) and s.siat_estado_facturacion=1
-	$sqlTipo order by f.cod_sucursal, f.nro_factura";
-	*/
 
 $sql="select s.nro_correlativo, DATE_FORMAT(s.fecha, '%d/%m/%Y'), s.monto_final, s.razon_social, s.nit, s.siat_cuf as nro_autorizacion, s.salida_anulada, '0' as cod_control, 
-(SELECT c.descripcion FROM ciudades c, almacenes a where a.cod_ciudad=c.cod_ciudad and a.cod_almacen=s.cod_almacen)nombre_ciudad, s.cod_tipo_doc from salida_almacenes s where YEAR(s.fecha)='$codAnio' and MONTH(s.fecha)='$codMes' and 
-s.cod_almacen in (select a.cod_almacen from almacenes a where a.cod_ciudad='$rpt_territorio') and s.siat_estado_facturacion=1 order by s.nro_correlativo";
+(SELECT c.descripcion FROM ciudades c, almacenes a where a.cod_ciudad=c.cod_ciudad and a.cod_almacen=s.cod_almacen)nombre_ciudad, s.cod_tipo_doc, s.siat_complemento, s.siat_usuario from salida_almacenes s where YEAR(s.fecha)='$codAnio' and MONTH(s.fecha)='$codMes' and 
+s.cod_almacen in (select a.cod_almacen from almacenes a where a.cod_ciudad in ($rpt_territorio)) and s.siat_estado_facturacion=1 order by s.nro_correlativo";
+
+//echo $sql;
 
 $resp=mysqli_query($enlaceCon,$sql);
 echo "<br><table align='center' class='table table-condensed' width='70%'>
 <thead>
 <tr class='bg-primary text-white'>
-<th class='bg-danger text-white'><small><small>Suc.</small></small></th>
-<th class='bg-danger text-white'><small><small>Tipo</small></small></th>
-<th class='bg-primary text-white'><small><small>ESP.</small></small></th>
 <th class='bg-primary text-white'><small><small>NRO.</small></small></th>
+<th class='bg-primary text-white'><small><small>ESP.</small></small></th>
 <th class='bg-primary text-white'><small><small>FECHA FACTURA</small></small></th>
 <th class='bg-primary text-white'><small><small>NRO. FACTURA</small></small></th>
 <th class='bg-primary text-white'><small><small>NRO. AUTORIZACION</small></small></th>
-<th class='bg-primary text-white'><small><small>ESTADO</small></small></th>
 <th class='bg-primary text-white'><small><small>NIT/CI CLIENTE</small></small></th>
+<th class='bg-primary text-white'><small><small>COMPLEMENTO</small></small></th>
 <th class='bg-primary text-white'><small><small>NOMBRE O RAZON SOCIAL</small></small></th>
-<th class='bg-primary text-white'><small><small>IMPORTE TOTAL VENTA<br>A</small></small></th>
-<th class='bg-primary text-white'><small><small>IMPORTE ICE/ IEHD/ IPJ/TASAS/ OTROS NO SUJETOS AL IVA <br>B </small></small></th>
-<th class='bg-primary text-white'><small><small>EXPORTACIONES Y OPERACIONES EXENTAS <br> C </small></small></th>
-<th class='bg-primary text-white'><small><small>VENTAS GRAVADAS A TASA CERO <br> D</small></small></th>
-<th class='bg-primary text-white'><small><small>SUBTOTAL <br> E = A - B - C - D </small></small></th>
-<th class='bg-primary text-white'><small><small>DESCUENTOS, BONIFICACIONES Y REBAJAS SUJETAS AL IVA <br> F</small></small></th>
-<th class='bg-primary text-white'><small><small>IMPORTE BASE PARA DEBITO FISCAL <br> G = E - F </small></small></th>
-<th class='bg-primary text-white'><small><small>DEBITO FISCAL <br> H = G * 13%</small></small></th>
+<th class='bg-primary text-white'><small><small>IMPORTE TOTAL VENTA</small></small></th>
+<th class='bg-primary text-white'><small><small>ICE</small></small></th>
+<th class='bg-primary text-white'><small><small>IEHD</small></small></th>
+<th class='bg-primary text-white'><small><small>IPJ</small></small></th>
+<th class='bg-primary text-white'><small><small>TASA</small></small></th>
+<th class='bg-primary text-white'><small><small>OTROS</small></small></th>
+<th class='bg-primary text-white'><small><small>EXPORT</small></small></th>
+<th class='bg-primary text-white'><small><small>VENTAGRABADA</small></small></th>
+<th class='bg-primary text-white'><small><small>SUBTOTAL</small></small></th>
+<th class='bg-primary text-white'><small><small>DESCUENTOS</small></small></th>
+<th class='bg-primary text-white'><small><small>GIFTCARD</small></small></th>
+<th class='bg-primary text-white'><small><small>IMPORTE BASE PARA DEBITO FISCAL</small></small></th>
+<th class='bg-primary text-white'><small><small>DEBITO FISCAL</small></small></th>
+<th class='bg-primary text-white'><small><small>ESTADO</small></small></th>
 <th class='bg-primary text-white'><small><small>CODIGO DE CONTROL</small></small></th>
+<th class='bg-primary text-white'><small><small>TIPOVENTA</small></small></th>
+<th class='bg-primary text-white'><small><small>USUARIO</small></small></th>
 </tr></thead><tbody>";
 
 $indice=1;
@@ -102,26 +104,36 @@ while($datos=mysqli_fetch_array($resp)){
 		  $nomTipo="M";	
 		}		
 	}
+	$complementoDocumento=$datos["siat_complemento"];
+	$usuarioCaja=$datos["siat_usuario"];
+
+
 	echo "<tr>
-	<td class='small' style='background:#E2E1DE;'>$nombreCiudad</td>
-    <td class='small' style='background:#E2E1DE;'>$nomTipo</td>
-	<td>3</td>
 	<td>$indice</td>
+	<td>3</td>
 	<td>$fecha</td>
 	<td>$nroFactura</td>
 	<td>$nroAutorizacion</td>
-	<td>$nombreEstadoF</td>
 	<td>$nit</td>
+	<td>$complementoDocumento</td>
 	<td>$razonSocial</td>
 	<td>$montoVentaFormat</td>
 	<td>0</td>
 	<td>0</td>
 	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
+	<td>0</td>
 	<td>$montoVentaFormat</td>
+	<td>0</td>
 	<td>0</td>
 	<td>$montoVentaFormat</td>
 	<td>$montoIVAFormat</td>
+	<td>$nombreEstadoF</td>
 	<td>$codigoControl</td>
+	<td>0</td>
+	<td>$usuarioCaja</td>
 	</tr>";
 	$indice++;
 }
