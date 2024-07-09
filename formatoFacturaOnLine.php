eb64c5bb14d21883c747a79c2b393dc688440d25
Marco Luna <lunagonzalesmarco@gmail.com>
2024-07-09 11:14:50 -0400

    cambios

--- formatoFacturaOnLine.php
+++ formatoFacturaOnLine.php
@@ -541,7 +541,7 @@ border-bottom: 1px solid #000;
         <table  style="width: 100%;">
             <tr>
                 <td align="center" width="35%"><br><br>
-                    <img src="imagenes/viacaucho.jpg" width="200"><br>
+                    <img src="imagenes/logo_loyola.jpg" width="100"><br>
                     <?=$sucursalTxt;?><br>
                     Punto de Venta <?=$siat_codigopuntoventa;?><br>
                     <?=utf8_decode($direccionTxt);?><br>
